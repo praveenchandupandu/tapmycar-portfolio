9a776f910a728062f39f9245bdd643fd61d08535
pramantechllc <pramantechllc@gmail.com>
2026-04-10 20:33:00 -0400

    Kill service worker cache v6

--- sw.js
+++ sw.js
@@ -1,23 +1,17 @@
-const CACHE_NAME = 'tapmycar-v1';
-const ASSETS = [
-  '/',
-  '/dashboard.html',
-  '/signin.html',
-  '/register.html',
-  '/app.css',
-  '/app.js'
-];
+﻿const CACHE_NAME = 'tapmycar-v6';
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
