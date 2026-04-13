0bad8e0dee8b570b3ae7d2b674a737f373265fd8
pramantechllc <pramantechllc@gmail.com>
2026-04-12 22:24:13 -0400

    Remove temp fix scripts

--- fix3.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('public/activity.html', 'utf8');
-const old = `  if (action === 'view' && scan.latitude && scan.longitude) {`;
-const neu = `  if ((action === 'view' || action === 'quick_message') && scan.latitude && scan.longitude && document.getElementById('detail-map-container')) {`;
-content = content.replace(old, neu);
-fs.writeFileSync('public/activity.html', content, 'utf8');
-console.log('done');
